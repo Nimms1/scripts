e66f3861c19103b2e16d9f7b2cd2ec3ace2a5f53
Nijisha <Nimms1@users.noreply.github.com>
2015-07-15 10:44:53 +0530

    Update install_mysql.sh

--- install_mysql.sh
+++ install_mysql.sh
@@ -6,8 +6,8 @@ apt-get update
 
 
 export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password welcome123 | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password welcome123 | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password password Welcome123 | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password Welcome123 | debconf-set-selections
 
 # install the LAMP stack
 apt-get -y install mysql-server 
@@ -16,7 +16,7 @@ sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my
 
 sudo service mysql restart
 
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'Welcome123'; flush privileges;" | mysql -u root -pWelcome123
 
 
-echo "create database VehicleRental" | mysql -u root -pwelcome123
+echo "create database VehicleRental" | mysql -u root -pWelcome123
