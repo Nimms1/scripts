958c137a8ad289e59c256eb5a574266a57f82b95
Nijisha <Nimms1@users.noreply.github.com>
2015-09-09 11:02:24 +0000

    Update presentation_conf.sh

--- presentation_conf.sh
+++ presentation_conf.sh
@@ -2,4 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
-docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.southeastasia.cloudapp.azure.com nijisha/mt-mvn-vr-presentation:3.6.2
+docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=devopsbusinessvminstance.southeastasia.cloudapp.azure.com nijisha/mt-mvn-vr-presentation:3.6.2
