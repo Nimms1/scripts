15a13f854cff048ac636574169013836137f348d
Nijisha <Nimms1@users.noreply.github.com>
2015-09-11 10:10:14 +0000

    Update business_conf.sh

--- business_conf.sh
+++ business_conf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get update
-curl -qO- https://get.docker.com/ | sh
+sudo apt-get install curl
+curl -sSL https://get.docker.com/ | sh
 #Run Docker
 docker run --name vapp -d -p 8080:8080 -e DB_IP=devopsdbvminstance.southeastasia.cloudapp.azure.com -e DB_PASSWORD=Welcome123 -e DB_PORT=3306 -e DB=VehicleRental nijisha/mt-vr-business:2.0
