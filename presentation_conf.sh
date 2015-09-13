4033ef707389108984d6b5808a76c56f85e4cae2
Nijisha <Nimms1@users.noreply.github.com>
2015-09-13 07:06:38 +0000

    Update presentation_conf.sh

--- presentation_conf.sh
+++ presentation_conf.sh
@@ -3,4 +3,4 @@ apt-get update
 sudo apt-get install curl
 curl -sSL https://get.docker.com/ | sh
 #Run Docker
-docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=devopsbusinessvminstance.southeastasia.cloudapp.azure.com nijisha/mt-mvn-vr-presentation:3.6.2
+docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.southeastasia.cloudapp.azure.com nijisha/mt-mvn-vr-presentation:3.6.2
