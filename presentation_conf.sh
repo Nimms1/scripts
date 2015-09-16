1bb7207ed90c707b059d3628611f2b1009e0f4bd
Nijisha <Nimms1@users.noreply.github.com>
2015-09-16 12:52:37 +0000

    Update presentation_conf.sh

--- presentation_conf.sh
+++ presentation_conf.sh
@@ -3,7 +3,7 @@ apt-get update
 echo sudo docker stop webp >> /home/docker/update.sh
 echo sudo docker rm webp >> /home/docker/update.sh
 echo sudo service docker restart >> /home/docker/update.sh
-echo sudo docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.southeastasia.cloudapp.azure.com $1 >> /home/docker/update.sh 
+echo 'sudo docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.southeastasia.cloudapp.azure.com $1' >> /home/docker/update.sh 
 sudo apt-get install curl
 curl -sSL https://get.docker.com/ | sh
 #Run Docker
